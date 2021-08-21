a50d525dcc944317447884f5582b270c67387570
skoroprox <skoroprox@gmail.com>
2021-08-21 21:29:10 +0300

    update code style

--- sandbox/src/main/java/second.java
+++ sandbox/src/main/java/second.java
@@ -1,7 +1,7 @@
 public class second {
 
-	public static void main (String[] args) {
-System.out.println("honey money");
- }
+    public static void main(String[] args) {
+        System.out.println("honey money");
+    }
 
 }
