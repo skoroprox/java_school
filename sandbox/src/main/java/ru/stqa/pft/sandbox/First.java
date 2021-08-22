da0755c828cd95b7111071d1ba79a16372aaa7e3
skoroprox <skoroprox@gmail.com>
2021-08-22 11:40:02 +0300

    create variables

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/First.java
@@ -0,0 +1,15 @@
+package ru.stqa.pft.sandbox;
+
+public class First {
+
+    public static void main(String[] args) {
+        String somebody = "world";
+        System.out.println("Hello, " + somebody + "!");
+
+        double l = 9;
+        double s = l*l;
+        System.out.println("Площадь квадрата со стороной " + l + " = " + s);
+    }
+
+}
+
