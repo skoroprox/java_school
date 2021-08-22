9de4c44839d7525f1e5f5956b8b2e5e6e948fc3d
skoroprox <skoroprox@gmail.com>
2021-08-22 13:44:29 +0300

    task2.1-2-3

--- sandbox/src/main/java/ru/stqa/pft/sandbox/First.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/First.java
@@ -1,5 +1,7 @@
 package ru.stqa.pft.sandbox;
 
+import static java.lang.Math.sqrt;
+
 public class First {
 
     public static void main(String[] args) {
@@ -10,8 +12,12 @@ public class First {
         Square s = new Square(5);
         System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
-        Rectangle r = new Rectangle(4,6);
+        Rectangle r = new Rectangle(4, 6);
         System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+
+        double p1 = 1;
+        double p2 = 8;
+        System.out.println("Расстояние между двумя точками" + " = " + distance(p1, p2));
 
 
     }
@@ -20,7 +26,11 @@ public class First {
         System.out.println("Hello, " + somebody + "!");
     }
 
+    public static double distance(double p1, double p2) {
+        return Math.sqrt(p1*p1 + p2*p2);
+    }
 
 
 }
+
 
