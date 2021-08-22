794c42fc4b26188111a9a84936cf7c08194fdf07
skoroprox <skoroprox@gmail.com>
2021-08-22 10:20:09 +0300

    update codeStyleSettings

--- sandbox/src/main/java/ru/stqa/pft/sandbox/first.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/first.java
@@ -3,7 +3,7 @@ package ru.stqa.pft.sandbox;
 public class first {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
-    }
+        System.out.println("Hello, world!");}
 
 }
+
