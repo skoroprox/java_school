14634a5a4af82c8b58e1bc9ff926245aab6aaeae
skoroprox <skoroprox@gmail.com>
2021-08-22 12:31:49 +0300

    methods: functions & objects

--- sandbox/src/main/java/ru/stqa/pft/sandbox/First.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/First.java
@@ -8,10 +8,10 @@ public class First {
         hello("Irina");
 
         Square s = new Square(5);
-        System.out.println("Площадь квадрата со стороной " + s.l + " = " + area(s));
+        System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
         Rectangle r = new Rectangle(4,6);
-        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + area(r));
+        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 
 
     }
@@ -20,11 +20,7 @@ public class First {
         System.out.println("Hello, " + somebody + "!");
     }
 
-    public static double area (Square s) {
-        return s.l * s.l;
-    }
- public static double area(Rectangle r) {
-        return r.a*r.b;
- }
+
+
 }
 
